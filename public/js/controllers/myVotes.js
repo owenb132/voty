20f1c57bf3dcf89107d06a06144780164d38f1ce
Jack Doyle <doylejj@tcd.ie>
2017-08-01 14:57:36 +0200

    Add correct poll owner text in My Polls view.

--- public/js/controllers/myVotes.js
+++ public/js/controllers/myVotes.js
@@ -1,24 +1,46 @@
 angular.module('Votapalooza')
-  .controller('VotesCtrl', function($scope, $rootScope, Account, Vote) {
+  .controller('VotesCtrl', function($scope, $rootScope, Account, Vote, Poll) {
     $scope.votes = [];
 
-    $scope.getVotes = function() {     
+    $scope.getVotes = function() {    
+        // Get all votes for this user 
         Account.myVotes()
             .then(function (response) {
                 var votes = response.data.votes;
+
+                // For each vote
                 async.each(votes, function(vote, callback) {
+
+                    // Get the poll information
                     Vote.getPoll(vote._id)
                         .then(function(response) {
-                            $scope.votes.push({ choice: vote.choice, poll: response.data.poll });
-                            console.log(response.data);
-                            callback(null, response.data.poll);
+                            vote.poll = response.data.poll;
+
+                            // Then get the owner information for this poll
+                            Poll.getUser(vote.poll._id)
+                                .then(function(response) {
+                                    vote.poll.owner = response.data.owner;
+                                    $scope.votes.push(vote);
+
+                                    // Let async know we've got all the information
+                                    callback(null, response.data.owner);
+                                }, function(response) {
+                                    $scope.messages = {
+                                        error: [response.data]
+                                    };
+                                });
                         }, function(response) {
+                            $scope.messages = {
+                                error: [response.data]
+                            };
+
                             callback(response);
                         });
-                })
-            }, function (response) {
-                $scope.error = `Error retrieving votes: ${response.status} ${response.statusText}`;
-                console.log(response);
+                });
+            }, function(response) {
+                $scope.messages = {
+                    error: [response.data]
+                };
             });
     }
 
