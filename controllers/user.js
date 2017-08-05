b479a3b37c9747c351724e9afa57050f8aa56531
Jack Doyle <doylejj@tcd.ie>
2017-08-05 02:52:51 +0200

    Refactor functionality to get current user's votes by moving logic to the server.

--- controllers/user.js
+++ controllers/user.js
@@ -6,6 +6,8 @@ var moment = require('moment');
 var request = require('request');
 var qs = require('querystring');
 var User = require('../models/User');
+var Poll = require('../models/Poll');
+var Vote = require('../models/Vote');
 
 function generateToken(user) {
   var payload = {
@@ -624,14 +626,95 @@ exports.authGithubCallback = function(req, res) {
   res.send('Loading...');
 };
 
+// Get all votes for the currently logged in user
 exports.myVotes = function(req, res) {
   var userId = req.user._id;
 
-  return User.findOne({_id: userId})
+  return User.findById(userId)
     .populate('votes').exec()
-    .then(respondWithResult(res))
+    .then(user => {
+      if (user.votes.length > 0) {
+        /* The vote objects only contain references to their respective
+        * polls' IDs. Need to make more queries to populate the data we
+        * need.
+        */
+        async.map(user.votes, getAllVoteInfo, (err, results) => {
+          if (results) {
+            res.status(200).send({ msg: 'Successfully retrieved votes.', votes: results });
+          } else if (err) {
+            res.status(500).send(err);
+          }
+        });
+      }
+    })
     .catch(handleError(res));
 };
+
+/* 
+* We need all the information for the vote so that we can display
+* it in the view in the format "[poll_name] by [poll_owner]".
+*/
+function getAllVoteInfo(vote, voteCallback, res) {
+  /* 
+  * We don't have access to the poll's owner, only the user who made
+  * the vote. We use waterfall to first get the poll itself, and then
+  * get the poll owner from that.
+  */
+  async.waterfall([
+    callback => {
+      getPollFromVote(vote, res, callback);
+    },
+
+    (poll, vote, callback) => {
+      getOwnerFromPoll(poll, vote, res, callback);
+    }
+  ], (err, result) => {
+    if (err) {
+      voteCallback(err);
+
+      /* 
+      * Result contains the vote object with the poll field populated.
+      * That poll field's owner field is in turn populated with the poll
+      * owner information.
+      */
+    } else if (result) {
+      voteCallback(null, result);
+    }
+  });
+}
+
+// Get poll information
+function getPollFromVote(vote, res, callback) {
+  Vote.findById(vote._id)
+    .populate('poll').exec()
+    .then(handleEntityNotFound(res))
+    .then(_vote => {
+      /*
+      * Add the base poll info to the passed in vote object,
+      * and send the updated vote object and the poll through
+      * to the next step.
+      */
+      vote.poll = _vote.poll;
+      callback(null, vote.poll, vote);
+    })
+    .catch(err => { callback(err) });
+}
+
+// Get poll owner information
+function getOwnerFromPoll(poll, vote, res, callback) {
+  Poll.findById(poll._id)
+    .populate('owner').exec()
+    .then(handleEntityNotFound(res))
+    .then(_poll => {
+      /* 
+      * Add the poll owner information to the passed in vote
+      * object, and we're done.
+      */
+      vote.poll.owner = _poll.owner;
+      callback(null, vote); 
+    })
+    .catch(err => { callback(err) }); 
+}
 
 exports.getVotes = function(req, res) {
   var userId = req.params.id;
--- public/js/controllers/myVotes.js
+++ public/js/controllers/myVotes.js
@@ -1,42 +1,18 @@
 angular.module('Votapalooza')
   .controller('VotesCtrl', function($scope, $rootScope, Account, Vote, Poll) {
+    $scope.loading = true;
     $scope.votes = [];
 
     $scope.getVotes = function() {    
         // Get all votes for this user 
         Account.myVotes()
-            .then(function (response) {
-                var votes = response.data.votes;
+            .then(function(response) {
+                $scope.loading = false;
+                $scope.votes = response.data.votes;
+                $scope.messages = {
+                    success: response.data.msg
+                };
 
-                // For each vote
-                async.each(votes, function(vote, callback) {
-
-                    // Get the poll information
-                    Vote.getPoll(vote._id)
-                        .then(function(response) {
-                            vote.poll = response.data.poll;
-
-                            // Then get the owner information for this poll
-                            Poll.getOwner(vote.poll._id)
-                                .then(function(response) {
-                                    vote.poll.owner = response.data.owner;
-                                    $scope.votes.push(vote);
-
-                                    // Let async know we've got all the information
-                                    callback(null, response.data.owner);
-                                }, function(response) {
-                                    $scope.messages = {
-                                        error: [response.data]
-                                    };
-                                });
-                        }, function(response) {
-                            $scope.messages = {
-                                error: [response.data]
-                            };
-
-                            callback(response);
-                        });
-                });
             }, function(response) {
                 $scope.messages = {
                     error: [response.data]
