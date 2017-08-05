474453a898e864de0ca3614df85dd03fbf789c7d
Jack Doyle <doylejj@tcd.ie>
2017-08-06 00:47:34 +0200

    Move app routes to its own file for better modularity.

--- server.js
+++ server.js
@@ -18,11 +18,6 @@ dotenv.load();
 // Models
 var User = require('./models/User');
 
-// Controllers
-var userController = require('./controllers/user');
-var pollController = require('./controllers/poll');
-var voteController = require('./controllers/vote');
-
 var app = express();
 
 // Connect to database
@@ -63,59 +58,7 @@ app.use(function(req, res, next) {
   }
 });
 
-// Account
-app.put('/account', userController.ensureAuthenticated, userController.accountPut);
-app.delete('/account', userController.ensureAuthenticated, userController.accountDelete);
-app.post('/signup', userController.signupPost);
-app.post('/login', userController.loginPost);
-app.post('/forgot', userController.forgotPost);
-app.post('/reset/:token', userController.resetPost);
-app.get('/unlink/:provider', userController.ensureAuthenticated, userController.unlink);
-
-// Facebook
-app.post('/auth/facebook', userController.authFacebook);
-app.get('/auth/facebook/callback', userController.authFacebookCallback);
-
-// Google
-app.post('/auth/google', userController.authGoogle);
-app.get('/auth/google/callback', userController.authGoogleCallback);
-
-// Twitter
-app.post('/auth/twitter', userController.authTwitter);
-app.get('/auth/twitter/callback', userController.authTwitterCallback);
-
-// Github
-app.post('/auth/github', userController.authGithub);
-app.get('/auth/github/callback', userController.authGithubCallback);
-
-// Api routes
-app.get('/api/polls', pollController.mostRecentN);
-app.get('/api/polls/:id', pollController.show);
-app.get('/api/polls/:id/user', pollController.getUser);
-app.post('/api/polls', pollController.create);
-app.patch('/api/polls/:id', pollController.patch);
-app.delete('/api/polls/:id', pollController.destroy);
-
-app.get('/api/votes', voteController.index);
-app.get('/api/votes/ip', voteController.findByIp);
-// app.get('/api/votes/:id', voteController.show);
-app.get('/api/votes/:id/user', voteController.getUser);
-app.get('/api/votes/:id/poll', voteController.getPoll);
-app.post('/api/votes', voteController.create);
-app.patch('/api/votes/:id', voteController.patch);
-app.delete('/api/votes/:id', voteController.destroy);
-
-app.get('/me/polls', userController.myPolls);
-app.get('/users/:id/polls', userController.getPolls);
-app.get('/me/votes', userController.myVotes);
-app.get('/users/:id/votes', userController.getVotes);
-
-app.get('/users/:id', userController.showUser);
-app.patch('/users/:id', userController.updateUser);
-
-app.get('*', function(req, res) {
-  res.redirect('/#' + req.originalUrl);
-});
+require('./routes')(app);
 
 // Production error handler
 if (app.get('env') === 'production') {
